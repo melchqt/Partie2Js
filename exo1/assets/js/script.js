eb2773aa1333afdb21cc6ba323cb573a01f90d10
melchqt <57181754+melchqt@users.noreply.github.com>
2019-11-29 16:23:08 +0100

    exo

---
+++ exo1/assets/js/script.js
@@ -0,0 +1,8 @@
+function img1(){
+  var imgVar = document.getElementById('img');
+  imgVar.src='assets/img/lapin.png';
+}
+function img2(){
+  var imgVariable = document.getElementById('img');
+  imgVariable.src='assets/img/azz.png';
+}
---
+++ exo2/assets/js/script.js
@@ -0,0 +1,11 @@
+//
+// var formvar = document.getElementById('form');
+// formvar.addEventListener('keyup',morgan);
+// function morgan() {
+//   alert(formvar.value);
+// }
+
+function lolilol() {
+  var lol = document.getElementById('form').value;
+  alert(lol)
+}
